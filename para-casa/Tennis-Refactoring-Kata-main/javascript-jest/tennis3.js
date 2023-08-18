6e4bc70781422d83340fee226f85910b80049a8b
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-08-17 22:26:51 -0300

    refactor: altera nome de variáveis

--- para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis3.js
+++ para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis3.js
@@ -1,20 +1,27 @@
 'use strict';
 
-function getScore(p1, p2) {
-    var s;
+function getScore(pointsP1, pointsP2) {
+    let s;
     const players = ["player1" , "player2"]
 
-    if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6)) {
-        var p = ["Love", "Fifteen", "Thirty", "Forty"];
-        s = p[p1];
-        return (p1 === p2) ? s + "-All" : s + "-" + p[p2];
+    if ((pointsP1 < 4 && pointsP2 < 4) && (pointsP1 + pointsP2 < 6)) {
+        let p = ["Love", "Fifteen", "Thirty", "Forty"];
+        s = p[pointsP1];
+        return (pointsP1 === pointsP2) ? s + "-All" : s + "-" + p[pointsP2];
     } else {
-        if (p1 === p2) {
-            return "Deuce";
-        }
-        s = p1 > p2 ? players[0] : players[1];
-        return ((p1 - p2) * (p1 - p2) === 1) ? "Advantage " + s : "Win for " + s;
+        if (pointsP1 === pointsP2) return "Deuce"
+
+        s = pointsP1 > pointsP2 ? players[0] : players[1];
+        return ((pointsP1 - pointsP2) * (pointsP1 - pointsP2) === 1) ? "Advantage " + s : "Win for " + s;
     }
+
+
+    // switch () {
+    //     case 1:
+
+    //     break,
+
+    //  }
 }
 
 module.exports = getScore;
