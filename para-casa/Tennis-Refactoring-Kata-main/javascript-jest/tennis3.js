9f7d71e276e3e741750dd84afc27a1468650630c
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-08-17 22:35:51 -0300

    refactor: refatora texto para template string

--- para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis3.js
+++ para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis3.js
@@ -7,21 +7,13 @@ function getScore(pointsP1, pointsP2) {
     if ((pointsP1 < 4 && pointsP2 < 4) && (pointsP1 + pointsP2 < 6)) {
         let pointDescription = ["Love", "Fifteen", "Thirty", "Forty"];
         scoreWinner = pointDescription[pointsP1];
-        return (pointsP1 === pointsP2) ? scoreWinner + "-All" : scoreWinner + "-" + pointDescription[pointsP2];
+        return (pointsP1 === pointsP2) ? `${scoreWinner}-All` : `${scoreWinner}-${pointDescription[pointsP2]}`;
     } else {
         if (pointsP1 === pointsP2) return "Deuce"
 
         scoreWinner = pointsP1 > pointsP2 ? players[0] : players[1];
-        return ((pointsP1 - pointsP2) * (pointsP1 - pointsP2) === 1) ? "Advantage " + scoreWinner : "Win for " + scoreWinner;
+        return ((pointsP1 - pointsP2) * (pointsP1 - pointsP2) === 1) ? `Advantage ${scoreWinner}` : `Win for ${scoreWinner}`;
     }
-
-
-    // switch () {
-    //     case 1:
-
-    //     break,
-
-    //  }
 }
 
 module.exports = getScore;
