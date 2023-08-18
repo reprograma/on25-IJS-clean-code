bc30c52d2af624db7674886d7d081a8a48df6018
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-08-17 22:03:59 -0300

    feat: altera os nomes da variáveis de jogadores e transforma em arrays

--- para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis3.js
+++ para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis3.js
@@ -2,8 +2,8 @@
 
 function getScore(p1, p2) {
     var s;
-    let p1N = "player1";
-    let p2N = "player2";
+    const players = ["player1" , "player2"]
+
     if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6)) {
         var p = ["Love", "Fifteen", "Thirty", "Forty"];
         s = p[p1];
@@ -12,7 +12,7 @@ function getScore(p1, p2) {
         if (p1 === p2) {
             return "Deuce";
         }
-        s = p1 > p2 ? p1N : p2N;
+        s = p1 > p2 ? players[0] : players[1];
         return ((p1 - p2) * (p1 - p2) === 1) ? "Advantage " + s : "Win for " + s;
     }
 }
