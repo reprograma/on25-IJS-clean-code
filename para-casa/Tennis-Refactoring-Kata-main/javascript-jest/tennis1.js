861979657be0607446ff9f9b63b230f67be5ed06
Katiene Souza <katienestudy@gmail.com>
2023-08-22 11:46:20 -0300

    add exercicio clean code

--- para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis1.js
+++ para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis1.js
@@ -1,53 +1,74 @@
-'use strict';
+class getScore {
 
-function getScore(m_score1, m_score2) {
-    var score = "";
-    var tempScore = 0;
-    if (m_score1 === m_score2) {
-        switch (m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
-    } else if (m_score1 >= 4 || m_score2 >= 4) {
-        var minusResult = m_score1 - m_score2;
-        if (minusResult === 1) {score = "Advantage player1";}
-        else if (minusResult === -1) {score = "Advantage player2";}
-        else if (minusResult >= 2) {score = "Win for player1";}
-        else {score = "Win for player2";}
-    } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) {tempScore = m_score1;}
-            else {
-                score += "-";
-                tempScore = m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
-        }
-    }
-    return score;
-}
+    scoreArray = [
+        "Love-All",
+        "Fifteen-All",
+        "Thirty-All",
+        "Deuce",
+    ];
+
+    constructor() {
+        this.score = "";
+        this.tempScore = 0;
+    };
+
+    getScoreByValidator(firstScore, secondScore) {
+        if (firstScore === secondScore) {
+            if (firstScore < 3 && secondScore < 3) {
+                this.score = this.scoreArray[firstScore];
+                
+                console.log(this.score);
+            } else if (firstScore >= 3 && secondScore >= 3) {
+                this.score = this.scoreArray[3];
+
+                console.log(this.score);
+            } else {
+                console.log("report two numbers!");
+            };
+        } else if (firstScore >= 4 || secondScore >= 4) {
+            let minusResult = firstScore - secondScore;
+
+            if (minusResult === 1) {
+                console.log("Advantage player1");
+
+            }else if (minusResult === -1) {
+                console.log("Advantage player2");
+
+            }else if (minusResult >= 2) {
+                console.log("Win for player1");
+
+            }else {
+               console.log("Win for player2");
+            };
+        } else {
+            for (var i = 1; i < 3; i++) {
+                if (i === 1) { tempScore = firstScore; }
+                else {
+                    score += "-";
+                    tempScore = secondScore;
+                };
+                
+                switch (tempScore) {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
+                };
+            };
+        };
+    };
+}; 
+
+const game = new getScore()
+game.getScoreByValidator(6, 6)
+
 
 module.exports = getScore;
