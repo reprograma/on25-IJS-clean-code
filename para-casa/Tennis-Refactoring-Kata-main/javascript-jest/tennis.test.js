4e9c972e7f39b2789c59678faf081f21074a1ebe
Ciane <100417980+ciane-pereira@users.noreply.github.com>
2023-08-28 17:39:03 -0300

    refatorado tennis1

--- para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis.test.js
+++ para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis.test.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 
 var each = require("jest-each").default;
 var getScore1 = require('./tennis1');
--- para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis1.js
+++ para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis1.js
@@ -1,53 +1,23 @@
-'use strict';
-
-function getScore(m_score1, m_score2) {
-    var score = "";
-    var tempScore = 0;
-    if (m_score1 === m_score2) {
-        switch (m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
+function getScore(score1, score2) {
+    let score = ["Love", "Fifteen", "Thirty", "Forty"];
+    
+    if (score1 === score2) {
+        if(score1 < 3) {
+        return `${score[score1]}-All`;            
+        } else {
+            return `Deuce`;
         }
-    } else if (m_score1 >= 4 || m_score2 >= 4) {
-        var minusResult = m_score1 - m_score2;
+    } else if (score1 >= 4 || score2 >= 4) {
+        let minusResult = score1 - score2;
         if (minusResult === 1) {score = "Advantage player1";}
         else if (minusResult === -1) {score = "Advantage player2";}
         else if (minusResult >= 2) {score = "Win for player1";}
         else {score = "Win for player2";}
     } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) {tempScore = m_score1;}
-            else {
-                score += "-";
-                tempScore = m_score2;
-            }
-            switch (tempScore) {
-                case 0:
-                    score += "Love";
-                    break;
-                case 1:
-                    score += "Fifteen";
-                    break;
-                case 2:
-                    score += "Thirty";
-                    break;
-                case 3:
-                    score += "Forty";
-                    break;
-            }
-        }
+        return `${score[score1]}-${score[score2]}`;
     }
     return score;
 }
 
 module.exports = getScore;
+
