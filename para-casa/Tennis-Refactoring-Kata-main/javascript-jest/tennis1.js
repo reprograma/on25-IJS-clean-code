85b4e54c2075f4ee13b0f4ee253bef3795e2fd11
marcelabino <134214698+marcelabino@users.noreply.github.com>
2023-09-04 11:56:40 -0300

    add: tennis 1 e tennis 2

--- para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis1.js
+++ para-casa/Tennis-Refactoring-Kata-main/javascript-jest/tennis1.js
@@ -4,33 +4,32 @@ function getScore(m_score1, m_score2) {
     var score = "";
     var tempScore = 0;
     if (m_score1 === m_score2) {
-        switch (m_score1) {
-            case 0:
-                score = "Love-All";
-                break;
-            case 1:
-                score = "Fifteen-All";
-                break;
-            case 2:
-                score = "Thirty-All";
-                break;
-            default:
-                score = "Deuce";
-                break;
-        }
+        return {
+            0: score = "Love-All",
+            1: score = "Fifteen-All",
+            2: score = "Thirty-All",
+            3: score = "Deuce",
+            4: score = "Deuce"
+        } [m_score1] || 'Error!'        
     } else if (m_score1 >= 4 || m_score2 >= 4) {
         var minusResult = m_score1 - m_score2;
-        if (minusResult === 1) {score = "Advantage player1";}
-        else if (minusResult === -1) {score = "Advantage player2";}
-        else if (minusResult >= 2) {score = "Win for player1";}
-        else {score = "Win for player2";}
-    } else {
-        for (var i = 1; i < 3; i++) {
-            if (i === 1) {tempScore = m_score1;}
-            else {
+        if (minusResult === 1) {
+            score = "Advantage player1";
+        } else if (minusResult === -1) {
+            score = "Advantage player2";
+        } else if (minusResult >= 2) {
+            score = "Win for player1";
+        } else {
+            score = "Win for player2";
+        }
+    } else { //quando m_score < 4
+        for (let i = 1; i < 3; i++) {
+            if (i === 1) {
+                tempScore = m_score1;
+            } else {
                 score += "-";
                 tempScore = m_score2;
-            }
+            } 
             switch (tempScore) {
                 case 0:
                     score += "Love";
@@ -43,7 +42,7 @@ function getScore(m_score1, m_score2) {
                     break;
                 case 3:
                     score += "Forty";
-                    break;
+                    break; 
             }
         }
     }
